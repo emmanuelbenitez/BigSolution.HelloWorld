5a1b739d42f2d951ee9f193944ad8f9479aaa40d
Emmanuel Benitez <FGM519@ores.be>
2020-01-09 11:10:53 +0100

    Add main applicaiton

---
+++ Application/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BigSolution.HelloWorld.Application
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World! {string.Join(" ", args)}");
+            Console.ReadKey();
+        }
+    }
+}
